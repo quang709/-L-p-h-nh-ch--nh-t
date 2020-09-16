75b72f0cf68b813cfc2138b2b7753883d79593e1
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-16 22:21:36 +0700

    Lớp hình chữ nhật

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lớp_hình_chữ_nhật
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height:");
+            double height = Double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("Your Rectangle \n" + rectangle.Display());
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+        }
+    }
+}
---
+++ Rectangle .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lớp_hình_chữ_nhật
+{
+    public class Rectangle
+    {
+        double width, height;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        //code below here
+
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.width + this.height) * 2;
+        }
+
+        public string Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+
+
+}
